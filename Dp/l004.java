6d0abb14d3d8dc6068d8e80351d3a9c55a698ad0
unknown <ishobhit3@gmail.com>
2021-09-27 23:49:21 +0530

    perm and comb

--- Dp/l004.java
+++ Dp/l004.java
@@ -29,12 +29,34 @@ public class l004 {
         return dp[n][tar] = count;
     }
 
+    public static int combination_Tab(int[] arr, int Tar, int[] dp) {
+            dp[0]=1 ;
+            for (int ele : arr){
+            for (int tar = 0; tar <= Tar; tar++)
+                if (tar - ele >= 0)
+                    dp[tar] += dp[tar - ele]; // permutation(arr, tar - ele, dp);
+        }
+        return dp[Tar];
+    }
+
+
     public static int permutation(int[] arr, int tar,int []dp) {
         if(tar==0) return dp[tar]=1 ;
         if(dp[tar]!=-1) return dp[tar] ;
         int count=0 ;
         for(int ele:arr) if(tar-ele>=0) count+=permutation(arr,tar-ele,dp) ;
         return dp[tar]=count ;
+    }
+
+
+    public static int permutation_Tab(int[] arr, int Tar, int[] dp) {
+        dp[0] = 1;
+        for (int tar = 1; tar <= Tar; tar++) {
+            for (int ele : arr)
+                if (tar - ele >= 0)
+                    dp[tar] += dp[tar - ele];
+        }
+        return dp[Tar];
     }
 
     public static void fill(int []dp) {
@@ -50,12 +72,10 @@ public class l004 {
     public static void target() {
         int[] arr = { 2, 3, 5, 7 };
         int tar = 10;
-        // int[] dp = new int[tar + 1];
-        int [][]dp=new int[arr.length+1][tar+1] ;
-        fill2D(dp) ;
-        // System.out.println(permutation(arr, tar, dp,arr.length));
-        System.out.println(combination(arr, tar, dp, arr.length));
-        display2D(dp);
+        int[] dp = new int[tar + 1];
+        // fill(dp) ;
+        System.out.println(permutation_Tab(arr, tar, dp));
+        display(dp) ;
     }
    public static void main(String[] args) {
     target() ;
